e24973a81823046dc2096dcd552d755784073a59
minhtai339 <151501601+minhtai339@users.noreply.github.com>
2023-11-20 13:43:30 +0700

    Add files via upload

---
+++ todolist/components/AssetExample.js
@@ -0,0 +1,34 @@
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+export default function AssetExample() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+        
+        Nguyễn Minh Tài 24TH
+
+        210101003@student.bdu.edu.vn
+      </Text>
+      <Image style={styles.logo} source={require('../assets/tt.jpg')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+  },
+  paragraph: {
+    margin: 24,
+    marginTop: 0,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  logo: {
+    height: 168,
+    width: 128,
+  }
+});
